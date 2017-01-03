35a5b686009df81e639857a768c4b295081297d0
Samuel Tardieu <sam@rfc1149.net>
2017-01-03 23:56:23 +0100

    Better error handling

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,21 @@ extern crate mysql;
 extern crate rand;
 
 use algo::*;
+use errors::*;
 use ini::Ini;
 use loaders::*;
 use stats::*;
+use std::io::Write;
 use types::*;
 
 mod algo;
-mod errors;
 mod loaders;
 mod stats;
 mod types;
+
+mod errors {
+    error_chain! {}
+}
 
 fn display_stats(a: &Assignments) {
     let ranks = statistics(a);
@@ -35,15 +40,26 @@ fn display_stats(a: &Assignments) {
     }
 }
 
-fn main() {
+fn load() -> Result<Assignments> {
     let conf = Ini::load_from_file("rsolver.ini").expect("cannot load configuration file");
     let solver = conf.section(Some("solver".to_string())).expect("cannot find solver section");
     let loader = match solver.get("loader").unwrap_or(&"mysql".to_string()).as_str() {
         "mysql" => MysqlLoader {},
         other => panic!("unknown loader {}", other),
     };
-    let (students, projects) = loader.load(&conf).unwrap();
-    let mut assignments = Assignments::new(students, projects);
-    assign(&mut assignments);
-    display_stats(&assignments);
+    let (students, projects) = loader.load(&conf)?;
+    Ok(Assignments::new(students, projects))
+}
+
+fn main() {
+    match load() {
+        Ok(mut assignments) => {
+            assign(&mut assignments);
+            display_stats(&assignments);
+        },
+        Err(e) => {
+            let _ = writeln!(&mut std::io::stderr(), "Error: {:#?}", e);
+            std::process::exit(1);
+        }
+    }
 }
