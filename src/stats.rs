7410d353438892060a77192d82eae590fbacf25f
Samuel Tardieu <sam@rfc1149.net>
2017-03-18 12:45:47 +0100

    Add missing files

---
+++ src/stats.rs
@@ -0,0 +1,15 @@
+use types::*;
+
+pub fn statistics(a: &Assignments) -> Vec<usize> {
+    let mut ranks = vec![0; a.projects.len()];
+    for project in a.filter_projects(|p| a.is_open(p)) {
+        for &student in a.students_for(project) {
+            if let Some(rank) = a.rank_of(student, project) {
+                ranks[rank] += 1;
+            }
+        }
+    }
+    let latest = ranks.iter().rposition(|&n| n != 0).map_or(0, |n| n + 1);
+    ranks.truncate(latest);
+    ranks
+}
---
+++ src/types/mod.rs
@@ -0,0 +1,7 @@
+pub use self::assignments::*;
+pub use self::project::*;
+pub use self::student::*;
+
+mod assignments;
+mod project;
+mod student;
